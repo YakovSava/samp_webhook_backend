623c07aa800abaf1d5ddbf2e83d08f860257c6b1
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-03-09 17:48:54 +0500

    Version 0.2
    Beta version

--- backend.py
+++ backend.py
@@ -1,10 +1,10 @@
-import asyncio
+import asyncio # Импортируем асинхронность
 
-from sys import platform
-from random import randint
-from json import loads 
-from typing import Coroutine
-from aiohttp.web import Application, RouteTableDef, Response, Request
+from sys import platform # Импортируем опредление платформы
+from random import randint # Импортируем рандом
+from json import loads # Импортируем модуль для работы с JSON
+from typing import Coroutine # ДЛЯ АННОТАЦИИ
+from aiohttp.web import Application, RouteTableDef, Response, Request, run_app # Импортируем кучу всего из aiohttp.web
 
 subapp = Application()
 routes = RouteTableDef()
@@ -13,23 +13,61 @@ if platform == 'win32':
 	try: asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 	except: pass
 
-class NoneFunction: pass
+async def add_func(nickname:str) -> None:
+	pass
+	# В этой функции мы на основе никнейма ищем telegram id игрока и пишем ему что он вошёл на сервер
+
+async def remove_func(nickname:str) -> None:
+	pass
+	# В этой функции мы на основе никнейма ищем telegram id игрока и пишем ему что он вышел с сервера
 
 class Back:
 	
-	def __init__(self, function:Coroutine=NoneFunction, host:str='127.0.0.1', port:int=randint(1000, 9999), subapp_host:str='service'):
-		self.function = function
-		self.app = Application()
+	def __init__(self,
+		add_function:Coroutine=add_func,
+		remove_function:Coroutine=remove_func,
+		host:str='127.0.0.1',
+		port:int=randint(1000, 9999),
+		subapp_host:str='service',
+		app:Application=Application(),
+		loop:asyncio.AbstractEventLoop=asyncio.get_event_loop()
+	):
+		self.add_function = add_function
+		self.remove_function = remove_function
+		self.app = app
+		self.host = host
+		self.port = port
+		self.subapp_host = subapp_host
+		self.loop = loop
 	
 	@routes.get('/server/add?{data}')
 	async def server_get_add(self, request:Request):
-		try: data = loads(str(request.path).split('?')[1])
+		try: data = loads(str(request.url()).split('?')[1])
 		except: return Response(status=503)
 
-		self.function(data)
+		self.add_function(data)
 
 		return Response(body=1)
 
-	@routes.post('/server/add')
-	async def server_post_add(self,request:Request):
-		pass
+	@routes.get('/server/remove?{data}')
+	async def server_get_remove(self, request:Request):
+		try: data = loads(str(request.url()).split('?')[1])
+		except: return Response(status=503)
+
+		self.remove_function(data)
+
+		return Response(body=1)
+
+	async def _preset_application(self) -> None:
+		subapp.add_routes(routes)
+		self.app.add_subapp(self.subapp_host, subapp)
+
+	async def run_server(self, loop:asyncio.AbstractEventLoop=None) -> None:
+		await self._preset_application()
+
+		run_app(
+			self.app,
+			host=self.host,
+			port=self.port,
+			loop=(loop if loop is not None else self.loop)
+		)
